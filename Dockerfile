edba21ae3c23294a14b88a11ce0ce43f9acee2c3
breannabisnott <breanna.bisnott@gmail.com>
2025-02-19 01:47:02 -0500

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Use an official Python runtime as the base image
+FROM python:3.13-slim
+
+# Set environment variables to avoid .pyc files and to ensure unbuffered output
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE 1
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app/
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Expose the port FastAPI will run on (default: 8000)
+EXPOSE 8000
+
+# Command to run the FastAPI app using uvicorn
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8000"]
---
+++ app.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, Query
+import psycopg
+from psycopg.rows import dict_row
+from pydantic import BaseModel
+from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+class SensorData(BaseModel):
+    id: int = None 
+    device_id: str
+    time_stamp: datetime = None
+    temperature: float
+    flame: bool
+    flame_level: float
+    gas: bool
+    gas_concentration: float
+    oxygen_concentration: float
+
+def get_db_connection():
+    return psycopg.connect(dbname=os.getenv("DB_NAME"), host=os.getenv("DB_HOST"), password=os.getenv("DB_PASS"), user= os.getenv("DB_USER"), row_factory=dict_row)
+
+@app.post("/data")
+async def new_data(request:SensorData):
+    request.time_stamp = datetime.now()
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                insert into sensor_data
+                (device_id, time_stamp, temperature, flame, flame_level, gas, gas_concentration, oxygen_concentration) 
+                values
+                (%s, %s, %s, %s, %s, %s, %s, %s)
+                """, 
+                (request.device_id, request.time_stamp, request.temperature, request.flame, request.flame_level, request.gas, request.gas_concentration, request.oxygen_concentration)
+            )
+            conn.commit()
+    return {"message": "success"}
+
+@app.get("/data/{device_id}")
+async def get_all_data(device_id: str, page_size:int = Query(None, description="number of records returned"), page_number:int = Query(None, description="number of records to offset by")):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """select * from sensor_data where device_id = %s order by time_stamp limit %s offset %s*%s
+                """, (device_id, page_size, page_size, page_number)
+            )
+            sensordata = cur.fetchall()
+    return sensordata
