26f166f239d950e5824eccb6ac8da960d4f71b74
breannabisnott <breanna.bisnott@gmail.com>
2025-02-20 23:53:39 -0500

    edited origins

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [ "http://127.0.0.1:5501" , "http://localhost:5501"]
+origins = [ "http://127.0.0.1:5501" , "http://localhost:5501", "http://fyahalarm.com", "https://fyahalarm.com"]
 
 app.add_middleware(
     CORSMiddleware,
