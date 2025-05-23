80ebe52c8d01c22dd2793abbc20ae789ca9f0c85
breannabisnott <breanna.bisnott@gmail.com>
2025-05-22 22:45:14 -0500

    settings page functional

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [ "http://127.0.0.1:5501" , "http://localhost:5501", "http://fyahalarm.com", "https://fyahalarm.com"]
+origins = [ "http://127.0.0.1:5501" , "http://localhost:5501", "http://fyahalarm.com", "https://fyahalarm.com", "http://127.0.0.1:8000"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -48,6 +48,29 @@ class SensorData(BaseModel):
     lng: float
     accuracy: float
 
+class SettingsData(BaseModel):
+    # id: int = None 
+    alert_email: str
+    fire_email: str
+    hospital_email:str
+    temp_thresh: float
+    gas_thresh: float
+
+class UpdateAlertEmail(BaseModel):
+    alert_email: str
+
+class UpdateFireEmail(BaseModel):
+    fire_email: str
+
+class UpdateHospitalEmail(BaseModel):
+    hospital_email: str
+
+class UpdateTempThresh(BaseModel):
+    temp_thresh: float
+
+class UpdateGasThresh(BaseModel):
+    gas_thresh: float
+
 def get_db_connection():
     return psycopg.connect(dbname=os.getenv("DB_NAME"), host=os.getenv("DB_HOST"), password=os.getenv("DB_PASS"), user= os.getenv("DB_USER"), row_factory=dict_row)
 
@@ -57,14 +80,14 @@ SENDER_EMAIL = "bxscuit.0@gmail.com"
 SENDER_PASSWORD = "wauk ihqr hjml nuay"  # Use App Password if using Gmail
 RECEIVER_EMAIL = "breanna.bisnott@gmail.com"
 
-def send_email_alert(device_id: str):
+def send_email_alert(device_id: str, lat: float, lng:float, alert_email:str, fire_email:str, hospital_email:str):
     """Send an email alert when sensor value is 0."""
     subject = f"🚨 FIRE DETECTED by device: {device_id}!"
-    body = f"Warning! Device {device_id} has detected a flame. Please check the system immediately."
+    body = f"Warning! Device {device_id} has detected a flame. Please check the system immediately.\nDevice Location:\nLatitude: {lat}\nLongitude: {lng}"
     
     msg = MIMEMultipart()
     msg["From"] = SENDER_EMAIL
-    msg["To"] = RECEIVER_EMAIL
+    msg["To"] = alert_email, fire_email, hospital_email
     msg["Subject"] = subject
     msg.attach(MIMEText(body, "plain"))
 
@@ -72,8 +95,50 @@ def send_email_alert(device_id: str):
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
             server.starttls()
             server.login(SENDER_EMAIL, SENDER_PASSWORD)
-            server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, msg.as_string())
+            server.sendmail(SENDER_EMAIL, alert_email, msg.as_string())
+            server.sendmail(SENDER_EMAIL, fire_email, msg.as_string())
+            server.sendmail(SENDER_EMAIL, hospital_email, msg.as_string())
         print(f" Email alert sent for Device {device_id}")
+    except Exception as e:
+        print(f" Failed to send email: {e}")
+
+def send_temp_threshold_email(device_id: str, lat: float, lng:float, alert_email:str):
+    """Send an email alert when sensor value is 0."""
+    subject = f"🚨 Device: {device_id} has detected HIGH TEMPERATURE!"
+    body = f"Warning! Device {device_id} has detected a tempeature above your designated threshold. Please check the system immediately.\nDevice Location:\nLatitude: {lat}\nLongitude: {lng}"
+    
+    msg = MIMEMultipart()
+    msg["From"] = SENDER_EMAIL
+    msg["To"] = alert_email
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body, "plain"))
+
+    try:
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()
+            server.login(SENDER_EMAIL, SENDER_PASSWORD)
+            server.sendmail(SENDER_EMAIL, alert_email, msg.as_string())
+        print(f" Temp Email alert sent for Device {device_id}")
+    except Exception as e:
+        print(f" Failed to send email: {e}")
+
+def send_gas_threshold_email(device_id: str, lat: float, lng:float, alert_email:str):
+    """Send an email alert when sensor value is 0."""
+    subject = f"🚨 Device: {device_id} has detected HIGH GAS CONCENTRATION!"
+    body = f"Warning! Device {device_id} has detected a gas concentration above your designated threshold. Please check the system immediately.\nDevice Location:\nLatitude: {lat}\nLongitude: {lng}"
+    
+    msg = MIMEMultipart()
+    msg["From"] = SENDER_EMAIL
+    msg["To"] = alert_email
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body, "plain"))
+
+    try:
+        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
+            server.starttls()
+            server.login(SENDER_EMAIL, SENDER_PASSWORD)
+            server.sendmail(SENDER_EMAIL, alert_email, msg.as_string())
+        print(f" Temp Email alert sent for Device {device_id}")
     except Exception as e:
         print(f" Failed to send email: {e}")
 
@@ -107,7 +172,7 @@ async def send_email(pdf: UploadFile = File(...), email: str = Form(...)):
         return JSONResponse(content={"message": f"An unexpected error occurred: {str(e)}"}, status_code=500)
     
 @app.post("/data")
-async def new_data(request:SensorData):
+async def new_data(request:SensorData, settings:SettingsData):
     request.time_stamp = datetime.now()
     with get_db_connection() as conn:
         with conn.cursor() as cur:
@@ -122,8 +187,16 @@ async def new_data(request:SensorData):
             )
             conn.commit()
     if request.flame == 1:
-        send_email_alert(request.device_id)
-        return {"message": "Alert sent!", "sensor_id": request.device_id}
+        send_email_alert(request.device_id, request.lat, request.lng, settings.alert_email, settings.fire_email, settings.hospital_email)
+        return {"message": "Alert sent!", "sensor_id": request.device_id, "lat": request.lat, "lng": request.lng}
+
+    if request.temperature > settings.temp_thresh:
+        send_temp_threshold_email(request.device_id, request.lat, request.lng, settings.alert_email)
+        return {"message": "Alert sent!", "sensor_id": request.device_id, "lat": request.lat, "lng": request.lng}
+
+    if request.gas_concentration > settings.gas_thresh:
+        send_gas_threshold_email(request.device_id, request.lat, request.lng, settings.alert_email)
+        return {"message": "Alert sent!", "sensor_id": request.device_id, "lat": request.lat, "lng": request.lng}
     
     return {"message": "success"}
 
@@ -160,4 +233,89 @@ async def get_latest_data(device_id: str):
             latestdata = cur.fetchall()
     return latestdata
 
-#test
+@app.post("/settingsInfo")
+async def new_settings(settings:SettingsData):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                insert into settings_data
+                (alert_email, fire_email, hospital_email, temp_thresh, gas_thresh) 
+                values
+                (%s, %s, %s, %s, %s)
+                """, 
+                (settings.alert_email, settings.fire_email, settings.hospital_email, settings.temp_thresh, settings.gas_thresh)
+            )
+            conn.commit()
+    
+    return {"settings": "updated"}
+
+@app.patch("/update-alertEmail")
+async def update_alert_email(settings: UpdateAlertEmail):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                UPDATE settings_data SET alert_email = %s WHERE id=1;
+                """, 
+                (settings.alert_email,)
+            )
+            conn.commit()
+    
+    return {"message": "Alert Email Updated"}
+
+@app.patch("/update-fireEmail")
+async def update_fire_email(settings: UpdateFireEmail):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                UPDATE settings_data SET fire_email = %s WHERE id=1;
+                """, 
+                (settings.fire_email,)
+            )
+            conn.commit()
+    
+    return {"message": "Fire Department Email Updated"}
+
+@app.patch("/update-hospitalEmail")
+async def update_hospital_email(settings: UpdateHospitalEmail):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                UPDATE settings_data SET hospital_email = %s WHERE id=1;
+                """, 
+                (settings.hospital_email,)
+            )
+            conn.commit()
+    
+    return {"message": "Hospital Email Updated"}
+
+@app.patch("/update-tempThresh")
+async def update_temp_thresh(settings: UpdateTempThresh):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                UPDATE settings_data SET temp_thresh = %s WHERE id=1;
+                """, 
+                (settings.temp_thresh,)
+            )
+            conn.commit()
+    
+    return {"message": "Temp Thresh updated"}
+
+@app.patch("/update-gasThresh")
+async def update_gas_thresh(settings: UpdateGasThresh):
+    with get_db_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                UPDATE settings_data SET gas_thresh = %s WHERE id=1;
+                """, 
+                (settings.gas_thresh,)
+            )
+            conn.commit()
+    
+    return {"message": "Gas Threshold Updated"}
